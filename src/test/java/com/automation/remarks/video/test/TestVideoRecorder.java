bff0eb11a504ac3f2c4b63519b8cc8c94b29a8ad
sergey <sergey@gmail.com>
2016-05-03 20:16:42 +0300

    more tests were added

--- src/test/java/com/automation/remarks/video/test/TestVideoRecorder.java
+++ src/test/java/com/automation/remarks/video/test/TestVideoRecorder.java
@@ -1,7 +1,6 @@
 package com.automation.remarks.video.test;
 
 import com.automation.remarks.video.recorder.VideoRecorder;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.File;
@@ -20,30 +19,28 @@ public class TestVideoRecorder {
     public static final String VIDEO_FILE_NAME = "video_test";
     public static final String VIDEO_FOLDER_NAME = "recordings";
 
-    static LinkedList<File> files;
-
-    @BeforeClass
-    public static void recordVideo() {
+    public LinkedList<File> recordVideo() {
         VideoRecorder recorder = new VideoRecorder(VIDEO_FILE_NAME);
         recorder.start();
-        files = recorder.stop();
+        return recorder.stop();
     }
 
     @Test
     public void shouldBeListWithOneVideo() {
+        LinkedList<File> files = recordVideo();
         assertEquals(files.size(), 1);
     }
 
     @Test
     public void shouldBeVideoInRecordingsFolder() throws IOException {
-        File video = files.getFirst();
+        File video = recordVideo().getFirst();
         String folderName = video.getParentFile().getName();
         assertEquals(folderName, VIDEO_FOLDER_NAME);
     }
 
     @Test
     public void shouldBeExactVideoFileName() throws Exception {
-        String fileName = files.getFirst().getName();
+        String fileName = recordVideo().getFirst().getName();
         assertThat(fileName, startsWith(VIDEO_FILE_NAME));
     }
 }
