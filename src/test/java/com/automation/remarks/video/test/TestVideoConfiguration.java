2f63c8e8b8f1bddf2e4f5a7e2e54936c8eacfc2c
sergey <sergey@gmail.com>
2016-05-03 20:48:20 +0300

    more tests were added

--- src/test/java/com/automation/remarks/video/test/TestVideoConfiguration.java
+++ src/test/java/com/automation/remarks/video/test/TestVideoConfiguration.java
@@ -1,12 +1,14 @@
 package com.automation.remarks.video.test;
 
 import com.automation.remarks.video.recorder.VideoRecorder;
+import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 
 import java.io.File;
 import java.util.LinkedList;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 /**
  * Created by sergey on 5/3/16.
@@ -20,5 +22,16 @@ public class TestVideoConfiguration {
         recorder.start();
         LinkedList<File> files = recorder.stop();
         assertEquals(files.size(), 0);
+    }
+
+    @Test
+    public void testName() throws Exception {
+        final String path = "/home/sergey/VideoRecorder/video/";
+        System.setProperty("video.folder", path);
+        VideoRecorder recorder = new VideoRecorder("test_video");
+        recorder.start();
+        LinkedList<File> files = recorder.stop();
+        String canonicalPath = files.getFirst().getCanonicalPath();
+        assertThat(canonicalPath, CoreMatchers.startsWith(path));
     }
 }
