de1cc3f27549c83e8b3615916efe41c4917a780b
sergey <sergey@gmail.com>
2016-07-10 11:06:39 +0300

    Allure integration removed due to problems on some version. Another way of integration will be provided

--- core/src/main/java/com/automation/remarks/video/RecordingUtils.java
+++ core/src/main/java/com/automation/remarks/video/RecordingUtils.java
@@ -1,10 +1,6 @@
 package com.automation.remarks.video;
 
-import ru.yandex.qatools.allure.annotations.Attachment;
-
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.util.LinkedList;
 
 /**
@@ -16,26 +12,15 @@ public class RecordingUtils {
     }
 
     public static String doVideoProcessing(boolean successfulTest, LinkedList<File> recordings) {
-        if(recordings.size() == 0){
+        if (recordings.size() == 0) {
             return "";
         }
         if (!successfulTest) {
             System.err.println("Video recording\n" + recordings);
-            File video = recordings.getFirst();
-            attachment(video);
-            return video.getAbsolutePath();
+            return recordings.getFirst().getAbsolutePath();
         } else if (recordings.size() > 0) {
             recordings.getFirst().delete();
         }
         return "no recordings on success test";
-    }
-
-    @Attachment(value = "video", type = "video/avi")
-    public static byte[] attachment(File video) {
-        try {
-            return Files.readAllBytes(video.toPath());
-        } catch (IOException e) {
-            return new byte[0];
-        }
     }
 }
