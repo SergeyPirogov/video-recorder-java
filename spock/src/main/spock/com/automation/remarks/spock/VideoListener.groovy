ab9e557c7c5773299486922176fef3a77ae8edc8
SergeyPirogov <Semen4ik20>
2016-10-21 15:32:26 +0300

    shouldIntercept was added to spock feature

--- spock/src/main/spock/com/automation/remarks/spock/VideoListener.groovy
+++ spock/src/main/spock/com/automation/remarks/spock/VideoListener.groovy
@@ -1,9 +1,12 @@
 package com.automation.remarks.spock
 
 import com.automation.remarks.video.RecorderFactory
+import com.automation.remarks.video.RecordingUtils
+import com.automation.remarks.video.annotations.Video
 import com.automation.remarks.video.recorder.IVideoRecorder
 import com.automation.remarks.video.recorder.VideoRecorder
 import org.spockframework.runtime.AbstractRunListener
+import org.spockframework.runtime.model.ErrorInfo
 import org.spockframework.runtime.model.FeatureInfo
 
 /**
@@ -12,15 +15,33 @@ import org.spockframework.runtime.model.FeatureInfo
 class VideoListener extends AbstractRunListener {
 
     IVideoRecorder recorder
+    def isSuccess = true
 
     @Override
     void beforeFeature(FeatureInfo feature) {
-        recorder = RecorderFactory.getRecorder(VideoRecorder.conf().getRecorderType())
-        recorder.start()
+        isSuccess = true
+        if (shouldIntercept(feature)) {
+            recorder = RecorderFactory.getRecorder(VideoRecorder.conf().getRecorderType())
+            recorder.start()
+        }
+    }
+
+    @Override
+    void error(ErrorInfo error) {
+        isSuccess = false
     }
 
     @Override
     void afterFeature(FeatureInfo feature) {
-        recorder.stopAndSave(feature.name)
+        if (shouldIntercept(feature)) {
+            def fileName = RecordingUtils.getVideoFileName(feature.getFeatureMethod().getAnnotation(Video), feature.name)
+            def video = recorder.stopAndSave(fileName.replace(" ", "_"))
+            RecordingUtils.doVideoProcessing(isSuccess, video)
+        }
+    }
+
+    def static shouldIntercept(FeatureInfo feature) {
+        Video video = feature.getFeatureMethod().getAnnotation(Video)
+        return RecordingUtils.videoEnabled(video)
     }
 }
--- spock/src/test/groovy/com/automation/remarks/spock/test/SpockSpec.groovy
+++ spock/src/test/groovy/com/automation/remarks/spock/test/SpockSpec.groovy
@@ -1,5 +1,6 @@
 package com.automation.remarks.spock.test
 
+import com.automation.remarks.video.annotations.Video
 import com.automation.remarks.video.recorder.VideoRecorder
 import spock.lang.Specification
 
@@ -9,12 +10,13 @@ import spock.lang.Specification
 class SpockSpec extends Specification {
 
     def setupSpec(){
-        VideoRecorder.conf().withVideoFolder("./custom_folder")
+
     }
 
+    @Video
     def "demo test"() {
         expect:
         sleep(5000)
-        2 == 2
+        2 != 2
     }
 }
